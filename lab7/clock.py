d6215f7fdb75d24e4842afeb8763c352ddf52317
21BTen <98640569+21BTen@users.noreply.github.com>
2022-04-01 20:43:24 +0600

    Update clock.py

--- lab7/clock.py
+++ lab7/clock.py
@@ -1,50 +1,45 @@
 import pygame
-import datetime
 from math import sin, cos, pi
+import datetime
 
-
-size = 500
+size = 700
 center = size / 2, size / 2
-fps = 60
 
 pygame.init()
 screen = pygame.display.set_mode([size, size])
 clock = pygame.time.Clock()
-mickey = pygame.image.load('mickey.png')
 
-leftHand = pygame.transform.scale(pygame.image.load('left.png'), (246 // 2, 165 // 2))
-rightHand = pygame.transform.scale(pygame.image.load('right.png'), (196 // 2, 162 // 2))
+mickey = pygame.image.load('m.png')
+Left = pygame.transform.scale(pygame.image.load('left.png'), (770 // 2, 230 // 2))
+Right = pygame.transform.scale(pygame.image.load('right.png'), (594 // 2, 322 // 2))
 
-
-
-def rot_center(image, angle, x, y):
-    
-    rotated_image = pygame.transform.rotate(image, angle)
-    new_rect = rotated_image.get_rect(center = image.get_rect(center = (x, y)).center)
-
-    return rotated_image, new_rect
+def blitRotate(surf, image, pos, originPos, angle):
+    image_rect = image.get_rect(topleft = (pos[0]- originPos[0], pos[1] - originPos[1]))
+    #смещение центра для поворота
+    centerPivot = pygame.math.Vector2(pos) - image_rect.center
+    RotateOfsett = centerPivot.rotate(angle)
+    RotatedCenter = (pos[0] - RotateOfsett.x, pos[1] - RotateOfsett.y)
+    RotatedImage = pygame.transform.rotate(image, -angle)
+    RotatedImage_rect = RotatedImage.get_rect(center = RotatedCenter)
+    surf.blit(RotatedImage, RotatedImage_rect)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             exit()
-    screen.fill(pygame.Color('white'))
-    screen.blit(mickey, (size/2 - 577/2, size/2 - 433/2))
 
-    # time
+    screen.fill(pygame.Color('white'))
+    screen.blit(mickey, (size/2 - 1200/2, size/2 - 900/2))
+
     current_time = datetime.datetime.now()
     second = current_time.second
     minute = current_time.minute
 
-    # minute
-    theta = (minute + second / 60) * (360 / 60)
-    rot_center(rightHand, theta,196 // 2, 162 // 2 )
+    theta = (minute + second/60) * (360 / 60)
+    blitRotate(screen, Right, center, (Right.get_width() / 2 + 110, Left.get_height() / 2 + 75), theta + 75)
 
-    # second
     theta = second * (360 / 60)
-    rot_center(leftHand, theta,246 // 2, 165 // 2)
+    blitRotate(screen, Left, center, (Left.get_width() / 2 - 145, Left.get_height() / 2), theta - 87)
 
-
-    # display
     pygame.display.flip()
-    clock.tick(fps)
+    clock.tick(60)

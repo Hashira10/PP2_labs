cf3e63d0ab8ed9471fdff8c9c873ec5ba94b4e27
21BTen <98640569+21BTen@users.noreply.github.com>
2022-04-21 14:53:52 +0600

    Update clock.py

--- lab7/clock.py
+++ lab7/clock.py
@@ -13,12 +13,13 @@ mickey = pygame.image.load('m.png')
 Left = pygame.transform.scale(pygame.image.load('left.png'), (770 // 2, 230 // 2))
 Right = pygame.transform.scale(pygame.image.load('right.png'), (594 // 2, 322 // 2))
 
-def blitRotate(surf, image, pos, originPos, angle):
-    image_rect = image.get_rect(topleft = (pos[0]- originPos[0], pos[1] - originPos[1]))
+def blitRotate(surf, image, originPos, angle):
+    image_rect = image.get_rect(topleft = (center[0]- originPos[0], center[1] - originPos[1]))
     #смещение центра для поворота
-    centerPivot = pygame.math.Vector2(pos) - image_rect.center
+    centerPivot = pygame.math.Vector2(center) - image_rect.center
+
     RotateOfsett = centerPivot.rotate(angle)
-    RotatedCenter = (pos[0] - RotateOfsett.x, pos[1] - RotateOfsett.y)
+    RotatedCenter = (center[0] - RotateOfsett.x, center[1] - RotateOfsett.y)
     RotatedImage = pygame.transform.rotate(image, -angle)
     RotatedImage_rect = RotatedImage.get_rect(center = RotatedCenter)
     surf.blit(RotatedImage, RotatedImage_rect)
@@ -36,10 +37,10 @@ while True:
     minute = current_time.minute
 
     theta = (minute + second/60) * (360 / 60)
-    blitRotate(screen, Right, center, (Right.get_width() / 2 + 110, Left.get_height() / 2 + 75), theta)
+    blitRotate(screen, Right, (Right.get_width() / 2 + 110, Left.get_height() / 2 + 75), theta )
 
     theta = second * (360 / 60)
-    blitRotate(screen, Left, center, (Left.get_width() / 2 - 145, Left.get_height() / 2), theta)
+    blitRotate(screen, Left, (Left.get_width() / 2 - 145, Left.get_height() / 2), theta )
 
     pygame.display.flip()
     clock.tick(60)
